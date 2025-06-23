6d88fa09d3d3af57bfab13cfd67397f905566bc8
HarshaKhande <hkhande@gmail.com>
2025-06-23 12:25:19 +0530

    Update SearchPage.ts

--- cypress/e2e/steps/src/pages/duckduckgo/SearchPage.ts
+++ cypress/e2e/steps/src/pages/duckduckgo/SearchPage.ts
@@ -2,7 +2,7 @@ export default class SearchPage {
     private readonly CSS_RESULT_SEARCH = '#react-layout [data-testid="mainline"]'
 
     resultContain(callback: (text: string) => void): void {
-        cy.get(this.CSS_RESULT_SEARCH, { timeout: 20000 }).then(($elem) => {
+        cy.get(this.CSS_RESULT_SEARCH, { timeout: 40000 }).then(($elem) => {
             callback($elem.text())
         })
     }
